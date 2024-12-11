7f74ff7561e40e085434f6ff6e9c47b1290cc8cf
exlef <50216668+exlef@users.noreply.github.com>
2024-12-11 21:59:54 +0300

    draw circles with generated material

--- verlet-integration/Assets/Demo.cs
+++ verlet-integration/Assets/Demo.cs
@@ -5,11 +5,43 @@ using Ex;
 public class Demo : MonoBehaviour
 {
     Material mat;
+    Mesh mesh;
+    ComputeBuffer positionBuffer;
+    RenderParams rp;
 
     void Start()
     {
         mat = Utils.GenerateMaterial();
+        mesh = Utils.GenerateCircleMesh(1);
+
+        Vector2[] instanceWorldPositions = {
+            new(0, 0),
+            new(1.1f, 0),
+            new(2.2f, 0),
+        };
+        positionBuffer = new ComputeBuffer(instanceWorldPositions.Length, sizeof(float) * 2);
+        positionBuffer.SetData(instanceWorldPositions);
+
+        rp = new(mat)
+        {
+            worldBounds = new Bounds(Vector3.zero, 10000 * Vector3.one), // use tighter bounds
+            matProps = new MaterialPropertyBlock()
+        };
+        rp.matProps.SetBuffer("_PositionBuffer", positionBuffer);
     }
+
+    void Update()
+    {
+        Graphics.RenderMeshPrimitives(rp, mesh, 0, 3);
+    }
+
+    void OnDestroy()
+    {
+        positionBuffer.Dispose();
+        positionBuffer?.Release();
+        positionBuffer = null;
+    }
+
 
 
     struct Point
