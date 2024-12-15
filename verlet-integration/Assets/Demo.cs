ea4a436f03b95f3477c7375efd9e63aac83afa81
exlef <50216668+exlef@users.noreply.github.com>
2024-12-16 01:10:31 +0300

    improving verlet integration

--- verlet-integration/Assets/Demo.cs
+++ verlet-integration/Assets/Demo.cs
@@ -5,58 +5,47 @@ using System.Collections.Generic;
 
 public class Demo : MonoBehaviour
 {
+    [SerializeField] Bounds bounds;
     [SerializeField] Material circleMat;
     [SerializeField] Material lineMat;
 
     ExGraphics gfx;
-    ResizableArray<Point> points = new();
-    ResizableArray<Stick> sticks = new();
-    List<Vector2> pointPos = new();
-    List<Vector4> lines = new();
 
-    [SerializeField] Bounds bounds;
+    List<Point> points = new();
+    List<Stick> sticks = new();
 
     void Start()
     {
         gfx = new(circleMat, lineMat);
         
-        points.Add(new Point(0, -1));
-        points.Add(new Point(2, 0));
+        points.Add(new Point(-1, 1)); // top left
+        points.Add(new Point(1, 1)); // top right
+        points.Add(new Point(-1, -1)); // bottom left
+        points.Add(new Point(1, -1)); // bottom right
 
-        sticks.Add(new Stick(0, 1, points));
+
+        sticks.Add(new Stick(points[0], points[1]));
+        sticks.Add(new Stick(points[2], points[3]));
+        sticks.Add(new Stick(points[0], points[2]));
+        sticks.Add(new Stick(points[1], points[3]));
+        sticks.Add(new Stick(points[0], points[3]));
+
+        points[0].pos += new float2(1.0f, 0);
     }
 
     void FixedUpdate()
     {
-        for (int i = 0; i < points.Count; i++)
+        UpdatePoints();
+        for (int i = 0; i < 3; i++)
         {
-            points.array[i].Update(bounds, Time.deltaTime);
-        }
-
-        for (int i = 0; i < sticks.Count; i++)
-        {
-            sticks.array[i].Update();
+            UpdateSticks();
+            ConstrainPointsToWorldBounds();
         }
     }
 
     void Update()
     {
-        // render
-        pointPos.Clear();
-        for (int i = 0; i < points.Count; i++)
-        {
-            pointPos.Add(points.array[i].pos);
-        }
-        gfx.DrawCircles(pointPos);
-        lines.Clear();
-        for (int i = 0; i < sticks.Count; i++)
-        {
-            Vector2 p0 = points.array[sticks.array[i].pAindex].pos;
-            Vector2 p1 = points.array[sticks.array[i].pBindex].pos;
-
-            lines.Add(new Vector4(p0.x, p0.y, p1.x, p1.y));
-        }
-        gfx.DrawLines(lines);
+        Render();
     }
 
     void OnDestroy()
@@ -69,7 +58,53 @@ public class Demo : MonoBehaviour
         Gizmos.DrawWireCube(bounds.center, bounds.extents);
     }
 
-    struct Point
+    void UpdatePoints()
+    {
+        foreach (var point in points)
+        {
+            point.Update();
+        }
+    }
+
+    void UpdateSticks()
+    {
+        foreach (var stick in sticks)
+        {
+            stick.Update();
+        }
+    }
+
+    void ConstrainPointsToWorldBounds()
+    {
+        foreach (var point in points)
+        {
+            point.ConstrainWorldBounds(bounds);
+        }
+    }
+
+    void Render()
+    {
+        List<Vector2> pointPos = new();
+        List<Vector4> lines = new();
+        pointPos.Clear();
+        for (int i = 0; i < points.Count; i++)
+        {
+            pointPos.Add(points[i].pos);
+        }
+        gfx.DrawCircles(pointPos);
+
+        lines.Clear();
+        for (int i = 0; i < sticks.Count; i++)
+        {
+            Vector2 p0 = sticks[i].pointA.pos;
+            Vector2 p1 = sticks[i].pointB.pos;
+
+            lines.Add(new Vector4(p0.x, p0.y, p1.x, p1.y));
+        }
+        gfx.DrawLines(lines);
+    }
+
+    class Point
     {
         public float2 pos;
         float2 oldPos;
@@ -85,17 +120,20 @@ public class Demo : MonoBehaviour
             radius = 0.1f;
             bounce = 0.9f;
             friction = 0.999f;
-            gravity = new float2(0, -2.8f);
+            gravity = new float2(0, -0.1f);
         }
 
-        public void Update(Bounds bounds, float dt)
+        public void Update()
         {
-            pos += gravity * dt;
-
             float2 v = (pos - oldPos) * friction;
             oldPos = pos;
             pos += v;
+            pos += gravity;
+        }
 
+        public void ConstrainWorldBounds(Bounds bounds)
+        {
+            float2 v = (pos - oldPos) * friction;
 
             float maxX = bounds.center.x + bounds.extents.x / 2 - radius;
             float minX = bounds.center.x - bounds.extents.x / 2 + radius;
@@ -126,25 +164,23 @@ public class Demo : MonoBehaviour
         }
     }
 
-    struct Stick
+    class Stick
     {
-        ResizableArray<Point> points;
-        public int pAindex;
-        public int pBindex;
+        public Point pointA;
+        public Point pointB;
         float length;
 
-        public Stick(int pointAindex, int pointBindex, ResizableArray<Point> pointsArray)
+        public Stick(Point a, Point b)
         {
-            points = pointsArray;
-            pAindex = pointAindex;
-            pBindex  = pointBindex;
-            length = Vector2.Distance(pointsArray.array[pAindex].pos, pointsArray.array[pBindex].pos);
+            pointA = a;
+            pointB = b;
+            length = Vector2.Distance(pointA.pos, pointB.pos);
         }
 
         public void Update()
         {
-            Vector2 p0 = points.array[pAindex].pos;
-            Vector2 p1 = points.array[pBindex].pos;
+            Vector2 p0 = pointA.pos;
+            Vector2 p1 = pointB.pos;
 
             float dx = p1.x - p0.x;
             float dy = p1.y - p0.y;
@@ -159,8 +195,8 @@ public class Demo : MonoBehaviour
             p1.x += offsetX;
             p1.y += offsetY;
 
-            points.array[pAindex].pos = p0;
-            points.array[pBindex].pos = p1;
+            pointA.pos = p0;
+            pointB.pos = p1;
         }
     }
 }
