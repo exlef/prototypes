f1df20a535bc319e060108d363adfb28cbd4c0ad
exlef <50216668+exlef@users.noreply.github.com>
2024-12-23 13:55:21 +0300

    add monobehaviour version of point and stick

--- verlet-integration/Assets/SpiderWebDemo/Point.cs
+++ verlet-integration/Assets/SpiderWebDemo/Point.cs
@@ -4,28 +4,32 @@ namespace SpiderWeb
 {
     class Point : MonoBehaviour
     {
-        public Vector2 pos;
-        public Vector2 oldPos;
+        public Transform tr
+        {
+            get
+            {
+                if (tr == null)
+                {
+                    tr = GetComponent<Transform>();
+                }
+                return tr;
+            }
+            set{}
+        }
+        public Vector3 oldPos;
         public bool pinned;
         readonly float radius;
-
-        public void Init(float x, float y, bool isPinned = false)
-        {
-            pos = new Vector2(x, y);
-            pinned = isPinned;
-            oldPos = pos;
-        }
 
         public void Tick()
         {
             if (pinned)
             {
-                oldPos = pos; // otherwise the oldPos will be the its value when we pinned the point and when it got unpinned this will cause to point move unexpectedly since its oldPos is not updated correctly.
+                oldPos = (Vector2)tr.position; // otherwise the oldPos will be the its value when we pinned the point and when it got unpinned this will cause to point move unexpectedly since its oldPos is not updated correctly.
                 return;
             }
-            Vector2 v = pos - oldPos;
-            oldPos = pos;
-            pos += v;
+            Vector3 v = tr.position - oldPos;
+            oldPos = tr.position;
+            tr.position += v;
         }
 
         public void ConstrainWorldBounds(Bounds bounds)
@@ -37,8 +41,11 @@ namespace SpiderWeb
             float maxY = bounds.center.y + bounds.extents.y / 2 - radius;
             float minY = bounds.center.y - bounds.extents.y / 2 + radius;
 
+            Vector2 pos = Vector2.zero;
             pos.x = Mathf.Clamp(pos.x, minX, maxX);
             pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+            tr.position = (Vector3)pos;
         }
     }
 }
--- verlet-integration/Assets/SpiderWebDemo/Stick.cs
+++ verlet-integration/Assets/SpiderWebDemo/Stick.cs
@@ -2,40 +2,49 @@ using UnityEngine;
 
 namespace SpiderWeb
 {
+    [RequireComponent(typeof(LineRenderer))]
     class Stick : MonoBehaviour
     {
         public Point pointA;
         public Point pointB;
-        float length;
-
-        public void Init(Point a, Point b)
-        {
-            pointA = a;
-            pointB = b;
-            length = Vector2.Distance(pointA.pos, pointB.pos);
-        }
+        [ExReadOnly] [SerializeField] float length;
 
         public void Tick()
         {
-            float dx = pointB.pos.x - pointA.pos.x;
-            float dy = pointB.pos.y - pointA.pos.y;
-            float distance = Vector2.Distance(pointA.pos, pointB.pos);
+            float dx = pointB.tr.position.x - pointA.tr.position.x;
+            float dy = pointB.tr.position.y - pointA.tr.position.y;
+            float distance = Vector2.Distance(pointA.tr.position, pointB.tr.position);
             float difference = length - distance;
 
             float percent = difference / distance;
             float offsetX = dx * percent / 2;
             float offsetY = dy * percent / 2;
 
+            Vector3 posA = Vector3.zero;
+            Vector3 posB = Vector3.zero;
+
             if (!pointA.pinned)
             {
-                pointA.pos.x -= offsetX;
-                pointA.pos.y -= offsetY;
+                posA.x -= offsetX;
+                posA.y -= offsetY;
             }
             if (!pointB.pinned)
             {
-                pointB.pos.x += offsetX;
-                pointB.pos.y += offsetY;
+                posB.x += offsetX;
+                posB.y += offsetY;
             }
+        }
+
+        void OnValidate()
+        {
+            if(pointA == null || pointB == null) return;
+            var line = GetComponent<LineRenderer>();
+            var posA = pointA.transform.position;
+            var posB = pointB.transform.position;
+            length = Vector2.Distance(posA, posB);
+            line.positionCount = 2;
+            line.SetPosition(0, posA);
+            line.SetPosition(1, posB);
         }
     }
 }
