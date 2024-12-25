a72b260ee28c810771de085d25b4039d9428a372
exlef <50216668+exlef@users.noreply.github.com>
2024-12-25 14:20:47 +0300

    add the spider web to the scene

--- verlet-integration/Assets/SpiderWebDemo/Editor/StickAdditionEditor.cs
+++ verlet-integration/Assets/SpiderWebDemo/Editor/StickAdditionEditor.cs
@@ -16,8 +16,23 @@ namespace SpiderWeb
             Event e = Event.current;
             if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Space)
             {
-                Debug.Log("spaceeeeee");
                 e.Use(); // Consume the event
+                // get all the currentl selected items
+                var selections = Selection.gameObjects;
+                if(selections.Length != 2) return;
+                var p1 = selections[0].GetComponent<Point>();
+                var p2 = selections[1].GetComponent<Point>();
+                if(p1 == null || p2 == null) return;
+                string sticksParentName = "Sticks";
+                Transform sticks = GameObject.Find(sticksParentName).GetComponent<Transform>();
+                if (!sticks) Debug.LogWarning($"there is no game object called as {sticksParentName} in the hierarchy.");
+                if (!sticks) return;
+                var go = LevelTool.InstantiatePrefab("Assets/SpiderWebDemo/Stick.prefab", sticks);
+                if (!go) return;
+                var stick = go.GetComponent<Stick>();
+                stick.pointA = p1;
+                stick.pointB = p2;
+                stick.ForceToRefresh();
             }
         }
     }
