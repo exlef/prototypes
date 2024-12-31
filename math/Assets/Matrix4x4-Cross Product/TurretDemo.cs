b8ca9ef57821df30c808dbb76b1a75f44cf59893
exlef <50216668+exlef@users.noreply.github.com>
2024-12-31 15:22:05 +0300

    add turret demo

--- math/Assets/Matrix4x4-Cross Product/TurretDemo.cs
+++ math/Assets/Matrix4x4-Cross Product/TurretDemo.cs
@@ -5,14 +5,33 @@ namespace Matrix_DotProduct
     public class TurretDemo : MonoBehaviour
     {
         [SerializeField] Transform turret;
+        Camera cam;
+
+        void Start()
+        {
+            cam = Camera.main;            
+        }
 
         void Update()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
             var result = Physics.Raycast(ray, out RaycastHit hit);
             if(!result) return;
             turret.position = hit.point;
+            
+
+            Debug.DrawLine(cam.transform.position, hit.point);
+
+            var xAxis = Vector3.Cross(hit.normal, ray.direction).normalized;
+            var yAxis = hit.normal.normalized;
+            var zAxis = Vector3.Cross(xAxis, yAxis).normalized;
+
+            Debug.DrawRay(turret.position, xAxis, Color.red);
+            Debug.DrawRay(turret.position, yAxis, Color.green);
+            Debug.DrawRay(turret.position, zAxis, Color.blue);
+
+            turret.rotation = Quaternion.LookRotation(zAxis, yAxis);
         }
     }
 }
