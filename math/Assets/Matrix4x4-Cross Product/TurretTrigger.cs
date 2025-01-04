02e2fd182eb21c1e6ea65811d8de700e2453f7df
exlef <50216668+exlef@users.noreply.github.com>
2025-01-04 17:28:50 +0300

    modify contains function accordingly

--- math/Assets/Matrix4x4-Cross Product/TurretTrigger.cs
+++ math/Assets/Matrix4x4-Cross Product/TurretTrigger.cs
@@ -31,20 +31,26 @@ public class TurretTrigger : MonoBehaviour
         Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
 
         { // inner trigger drawing
-            Gizmos.DrawRay(center, innerLeftCorner);
-            Gizmos.DrawRay(center, innerRightCorner);
-            Gizmos.DrawRay(top, innerLeftCorner);
-            Gizmos.DrawRay(top, innerRightCorner);
+            // Gizmos.DrawRay(center, innerLeftCorner);
+            // Gizmos.DrawRay(center, innerRightCorner);
+            // Gizmos.DrawRay(top, innerLeftCorner);
+            // Gizmos.DrawRay(top, innerRightCorner);
 
-            Gizmos.DrawLine(center, top);
-            Gizmos.DrawLine(center + innerLeftCorner, top + innerLeftCorner);
-            Gizmos.DrawLine(center + innerRightCorner, top + innerRightCorner);
+            // Gizmos.DrawLine(center, top);
+            // Gizmos.DrawLine(center + innerLeftCorner, top + innerLeftCorner);
+            // Gizmos.DrawLine(center + innerRightCorner, top + innerRightCorner);
 
-            Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
-            Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
+            // Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
+            // Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
         }
 
-        Gizmos.color = Handles.color = Color.red;
+
+        bool contains = !Contains(forward, innerRadius ) && Contains(forward, innerRadius + outerRadius);
+
+        Gizmos.color = Handles.color = contains ? Color.green : Color.white;
+
+        Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
+        Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
 
         Gizmos.DrawLine(innerLeftCorner, outerLeftCorner);
         Gizmos.DrawLine(innerRightCorner, outerRightCorner);
@@ -66,7 +72,7 @@ public class TurretTrigger : MonoBehaviour
         head.rotation = Quaternion.Slerp(currentRot, targetRot, Time.deltaTime);
     }
 
-    bool Contains(Vector3 forward)
+    bool Contains(Vector3 forward, float radius)
     {
         Vector3 tp = transform.InverseTransformPoint(target.position);
         
@@ -80,7 +86,7 @@ public class TurretTrigger : MonoBehaviour
         float angleTo  = Mathf.Acos(dot) * Mathf.Rad2Deg;
         if(angleTo  > angle) return false;
 
-        if(toTargetProjectedToXZPlane.magnitude > innerRadius) return false;
+        if(toTargetProjectedToXZPlane.magnitude > radius) return false;
 
         return true;;
     }
