88d7fbcbca2aeebb39fafe9ab77d1adab4a4b6ba
exlef <50216668+exlef@users.noreply.github.com>
2025-01-05 14:02:56 +0300

    working on trigger types

--- math/Assets/Matrix4x4-Cross Product/TurretTrigger.cs
+++ math/Assets/Matrix4x4-Cross Product/TurretTrigger.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class TurretTrigger : MonoBehaviour
 {
+    enum TurretTriggerE
+    {
+        CylindricalSector,
+        Spherical,
+        SphericalSector,
+    }
+    [SerializeField] TurretTriggerE triggerType; 
     [SerializeField] Transform target;
     [SerializeField] Transform head;
     [SerializeField] [Min(0)] float innerRadius = 1;
@@ -15,54 +22,91 @@ public class TurretTrigger : MonoBehaviour
     {
         Vector3 up = Vector3.up;
         Vector3 forward = Vector3.forward;
+        Vector3 right = Vector3.right;
 
         Vector3 center = Vector3.zero; // since we are going to draw in local space center is Vector3(0,0,0)
         Vector3 top = up * height;
+        
+        
+        switch (triggerType)
+        {
+            case TurretTriggerE.CylindricalSector:
+                {
+                    Quaternion q = Quaternion.Euler(0, angle / 2, 0);
+                    Vector3 leftDir = Quaternion.Inverse(q) * forward;
+                    Vector3 rightDir = q * forward;
 
-        Quaternion q = Quaternion.Euler(0, angle/2, 0);
-        Vector3 leftDir = Quaternion.Inverse(q) * forward;
-        Vector3 rightDir = q * forward;
+                    Vector3 innerLeftCorner = leftDir * innerRadius;
+                    Vector3 innerRightCorner = rightDir * innerRadius;
+                    Vector3 outerLeftCorner = innerLeftCorner + leftDir * outerRadius;
+                    Vector3 outerRightCorner = innerRightCorner + rightDir * outerRadius;
 
-        Vector3 innerLeftCorner = leftDir * innerRadius;
-        Vector3 innerRightCorner = rightDir * innerRadius;
-        Vector3 outerLeftCorner = innerLeftCorner + leftDir * outerRadius;
-        Vector3 outerRightCorner = innerRightCorner + rightDir * outerRadius;
+                    Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
 
-        Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
+                    { // inner trigger drawing
+                      // Gizmos.DrawRay(center, innerLeftCorner);
+                      // Gizmos.DrawRay(center, innerRightCorner);
+                      // Gizmos.DrawRay(top, innerLeftCorner);
+                      // Gizmos.DrawRay(top, innerRightCorner);
 
-        { // inner trigger drawing
-            // Gizmos.DrawRay(center, innerLeftCorner);
-            // Gizmos.DrawRay(center, innerRightCorner);
-            // Gizmos.DrawRay(top, innerLeftCorner);
-            // Gizmos.DrawRay(top, innerRightCorner);
+                        // Gizmos.DrawLine(center, top);
+                        // Gizmos.DrawLine(center + innerLeftCorner, top + innerLeftCorner);
+                        // Gizmos.DrawLine(center + innerRightCorner, top + innerRightCorner);
 
-            // Gizmos.DrawLine(center, top);
-            // Gizmos.DrawLine(center + innerLeftCorner, top + innerLeftCorner);
-            // Gizmos.DrawLine(center + innerRightCorner, top + innerRightCorner);
+                        // Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
+                        // Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
+                    }
 
-            // Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
-            // Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
+
+                    bool contains = !Contains(forward, innerRadius) && Contains(forward, innerRadius + outerRadius);
+
+                    Gizmos.color = Handles.color = contains ? Color.green : Color.white;
+
+                    Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
+                    Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
+
+                    Gizmos.DrawLine(innerLeftCorner, outerLeftCorner);
+                    Gizmos.DrawLine(innerRightCorner, outerRightCorner);
+                    Gizmos.DrawLine(top + innerLeftCorner, top + outerLeftCorner);
+                    Gizmos.DrawLine(top + innerRightCorner, top + outerRightCorner);
+
+                    Gizmos.DrawLine(outerLeftCorner, top + outerLeftCorner);
+                    Gizmos.DrawLine(outerRightCorner, top + outerRightCorner);
+
+                    Handles.DrawWireArc(center, up, leftDir, angle, outerRadius + innerRadius);
+                    Handles.DrawWireArc(top, up, leftDir, angle, outerRadius + innerRadius);
+                }                
+                break;
+            case TurretTriggerE.Spherical:
+                Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
+                Gizmos.color = SphereTriggerContains() ? Color.green : Color.white;
+                Gizmos.DrawWireSphere(center, innerRadius);
+                Gizmos.DrawWireSphere(center, outerRadius);
+                Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
+                break;
+            case TurretTriggerE.SphericalSector:
+                {
+                    Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
+                    // Gizmos.DrawWireSphere(center, outerRadius);
+                    Vector3 verticalArcStartingPoint = Quaternion.Euler(-angle / 2, 0, 0) * forward * outerRadius;
+                    Handles.DrawWireArc(center, right, Quaternion.Euler(-angle / 2, 0, 0) * forward, angle, outerRadius);
+                    Handles.DrawWireArc(center, up, Quaternion.Euler(0, -angle / 2, 0) * forward, angle, outerRadius);
+                }                
+                break;
         }
-
-
-        bool contains = !Contains(forward, innerRadius ) && Contains(forward, innerRadius + outerRadius);
-
-        Gizmos.color = Handles.color = contains ? Color.green : Color.white;
-
-        Handles.DrawWireArc(center, up, leftDir, angle, innerRadius);
-        Handles.DrawWireArc(top, up, leftDir, angle, innerRadius);
-
-        Gizmos.DrawLine(innerLeftCorner, outerLeftCorner);
-        Gizmos.DrawLine(innerRightCorner, outerRightCorner);
-        Gizmos.DrawLine(top + innerLeftCorner, top + outerLeftCorner);
-        Gizmos.DrawLine(top + innerRightCorner, top + outerRightCorner);
-
-        Gizmos.DrawLine(outerLeftCorner, top  + outerLeftCorner);
-        Gizmos.DrawLine(outerRightCorner, top + outerRightCorner);
-
-        Handles.DrawWireArc(center, up, leftDir, angle, outerRadius + innerRadius);
-        Handles.DrawWireArc(top, up, leftDir, angle, outerRadius + innerRadius);
     }
+
+    void OnValidate()
+    {
+        if(innerRadius > outerRadius)
+            innerRadius = outerRadius - 0.1f;
+    }
+
+    bool SphereTriggerContains()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+        return dist > innerRadius && dist < outerRadius;
+    }    
 
     void RotateHeadToTarget()
     {
