ca7b440e3724f850696cee33c345c58483bbed20
exlef <50216668+exlef@users.noreply.github.com>
2025-01-09 23:32:27 +0300

    puzzle-1 improve segment movement

--- puzzle-1/Assets/Segment.cs
+++ puzzle-1/Assets/Segment.cs
@@ -7,14 +7,15 @@ public class Segment : MonoBehaviour
 
     public void Move(Vector2 centerCircle, float radius)
     {
-        if(Vector2.Distance(transform.position, centerCircle) <= radius)
-        {
-            hasArrived = true;
-            return;
-        }
-        // how this work? I expect this to move in world space up but it moves 
-        // in loccal space up (at least this is what I understand. I need to look into this)
+        if(hasArrived) return;
+        
         transform.Translate(Vector2.up * (speed * Time.deltaTime));
+
+        if (Vector2.Distance(transform.position, centerCircle) <= radius)
+        {
+            transform.position = (Vector3)centerCircle + (-transform.up * radius);
+            hasArrived = true;
+        }
     }
 
     public void SetParent(Transform tr)
@@ -28,10 +29,5 @@ public class Segment : MonoBehaviour
         transform.position = centerPos + (sectorDir * dist);
         transform.up = -sectorDir;
         transform.parent = centerCirclTr;
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(transform.position, transform.up);
     }
 }
