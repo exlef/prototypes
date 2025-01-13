6245813044340fabaad9f896b7fbd29f39ed2784
exlef <50216668+exlef@users.noreply.github.com>
2025-01-13 15:49:58 +0300

    working on donut

---
+++ math/Assets/FinalAssignments/2_Spring/Donut.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEditor;
+
+public class Donut : MonoBehaviour
+{
+    [SerializeField] float radius = 1;
+    [SerializeField] int resolution = 32;
+    [SerializeField] int layerCount = 2;
+    [SerializeField] float heightMultiplier = 0.5f;
+    [SerializeField] Color bottom = Color.black;    
+    [SerializeField] Color top = Color.white;
+    
+    Vector3[] points;
+    Color[] colors;
+    
+    private void OnDrawGizmos()
+    {
+        // Handles.matrix = transform.localToWorldMatrix;
+        
+        points = new Vector3[resolution * layerCount];
+        colors = new Color[resolution * layerCount];
+        var angle = 360.0f / resolution;
+
+        Vector3 center = transform.position;
+        for (int j = 0; j < layerCount; ++j)
+        {
+            center.x += 1f;
+            center.y += 1f;
+            for (int i = 0; i < resolution; i++)
+            {
+                var rotation = Quaternion.Euler(0, angle * i, 0);
+                var dir = transform.right;
+                dir = (rotation * dir).normalized;
+                var pos = dir * radius;
+                pos.y = Mathf.Lerp(j * heightMultiplier, (j+1) * heightMultiplier, i / (float)resolution);
+
+                pos += center;
+                
+                points[j * resolution +  i] = pos;
+            }    
+        }
+
+        for (var i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.Lerp(bottom, top, i / (float)colors.Length);
+        }
+
+        Handles.DrawAAPolyLine(5, colors,points);
+    }
+}
