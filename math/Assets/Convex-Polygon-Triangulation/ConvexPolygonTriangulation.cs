0fd2f51526d617df39f4050ccc8023ef2a638b4b
exlef <50216668+exlef@users.noreply.github.com>
2025-02-07 22:40:45 +0300

    math - generate a quad

--- math/Assets/Convex-Polygon-Triangulation/ConvexPolygonTriangulation.cs
+++ math/Assets/Convex-Polygon-Triangulation/ConvexPolygonTriangulation.cs
@@ -6,30 +6,24 @@ public class ConvexPolygonTriangulation : MonoBehaviour
 {
     [SerializeField] Transform[] pointsTr;
     Mesh mesh;
+
     void Start()
     {
         mesh = new Mesh();
         
         var points = new Vector3[pointsTr.Length];
-        for (int i = 0; i < pointsTr.Length; i++)
-        {
-            points[i] = pointsTr[i].position;
-        }
+        for (int i = 0; i < pointsTr.Length; i++) points[i] = pointsTr[i].position;
         mesh.vertices = points;
 
-        int[] triangles = new int[pointsTr.Length];
-        for (int i = 0; i < points.Length; i += 4)
+        int[] triangles = new int[6]; // how can I calculate the triangles count for given vertex count.
+        for (int i= 0, k = 0 ; i < points.Length; i += 2, k += 3)
         {
-            Debug.Log(i);
-            triangles[i] = i;
-            triangles[i+1] = i+1;
-            if (i + 2 > points.Length - 1) triangles[i + 2] = 0;
-            else triangles[i + 2] = i+2;
+            triangles[k + 0] = i;
+            triangles[k + 1] = i + 1 <= points.Length - 1 ? i + 1 : 0;
+            triangles[k + 2] = i + 2 <= points.Length - 1 ? i + 2 : 0;
         }
-
-        Debug.Log(triangles.Length);
         mesh.triangles = triangles;
-
+        
         GetComponent<MeshFilter>().mesh = mesh;
     }
 
